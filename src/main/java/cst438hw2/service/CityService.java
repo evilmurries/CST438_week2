f335d405be2d909df47b87bb0566fbdf537d1a00
evilmurries <cpiwarski00@gmail.com>
2020-03-14 08:09:44 -0700

    initial commit

---
+++ src/main/java/cst438hw2/service/CityService.java
@@ -0,0 +1,32 @@
+package cst438hw2.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cst438hw2.domain.*;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
+	@Autowired
+	private CountryRepository countryRepository;
+	
+	@Autowired
+	private WeatherService weatherService;
+	
+	public CityInfo getCityInfo(String cityName) {
+		
+		// TODO your code goes here
+		// delete the following line
+		return null; 
+	}
+	
+}
---
+++ src/test/java/com/example/demo/Cst438week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438week2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
